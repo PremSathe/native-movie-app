ba9d81a2a550b8e0921d9c5dc02371d3bc6b8796
PremSathe <premsathe78@gmail.com>
2025-05-20 16:08:33 +0530

    movie-app

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,10 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
-
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome!</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-[#123123] font-bold">Welcome!</Text>
+      <Link href="/app/onboarding">onboarding</Link>{" "}
     </View>
   );
 }
